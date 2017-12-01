6044d36973cc26451e1ed66c331488ac7bc267c1
Lanxin123 <337544818@qq.com>
2017-12-01 21:03:48 +0800

    The 5 days

---
+++ STL源码剖析/Dante-stl-source/iterators/Test_for_return_value.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+template <class T>
+struct MyIter
+{
+	typedef T  value_type;
+	T*ptr;
+	MyIter(T*p = 0) : ptr(p) { }
+	T& operator*()const { return *ptr; }
+};
+
+template <class I>
+typename I ::value_type
+func(I ite)
+{
+	return *ite;
+}
+
+int main()
+{
+	MyIter<int> ite(new int(8));
+	cout << func(ite);
+}
+
+
