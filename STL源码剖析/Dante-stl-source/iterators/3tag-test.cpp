bbc9369b806edc8c9954225803e32b67fd4515ad
Lanxin123 <337544818@qq.com>
2017-12-02 22:26:18 +0800

    The 6 days

---
+++ STL源码剖析/Dante-stl-source/iterators/3tag-test.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+struct B {};
+struct D1 : public B {};
+struct D2 : public D1 {};
+
+template<class I>
+void func(I& p, B)
+{
+	cout << "B version" << endl;
+}
+
+template<class I>
+void func(I& p, D2)
+{
+	cout << "D2 version" << endl;
+}
+
+int main()
+{
+	int* p;
+	func(p, B());
+	func(p, D1());
+	func(p, D2());
+	auto* a = new B();
+	func(p, *a);
+
+}
